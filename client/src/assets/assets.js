bb905151301d7c3ed7e9a768dc03a0f33db932fe
Ananya Kr. Lohia <146734083+ananylohia@users.noreply.github.com>
2025-08-25 21:20:13 +0530

    Update assets.js

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -15,10 +15,15 @@ export const assets = {
 }
 
 export const dummyTrailers = [
+
     {
-        image: "https://img.youtube.com/vi/WpW36ldAqnM/maxresdefault.jpg",
-        videoUrl: 'https://www.youtube.com/watch?v=WpW36ldAqnM'
+        image: "https://img.youtube.com/vi/73_1biulkYk/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=73_1biulkYk'
     },
+    // {
+    //     image: "https://img.youtube.com/vi/WpW36ldAqnM/maxresdefault.jpg",
+    //     videoUrl: 'https://www.youtube.com/watch?v=WpW36ldAqnM'
+    // },
     {
         image: "https://img.youtube.com/vi/-sAOWhvheK8/maxresdefault.jpg",
         videoUrl: 'https://www.youtube.com/watch?v=-sAOWhvheK8'
